706e45254a7c1a12a566496174a883fcd426c6cb
Mohammad Lummim Sarker <lamimsarker@gmail.com>
2024-05-05 17:25:33 +0200

    latest update

--- AuctionHouse.py
+++ AuctionHouse.py
@@ -1,18 +1,11 @@
 from sre_compile import isstring
-
-
 class AuctionHouse:
-    comission_bids = []
-    live_bidder={"C","750"}
-    registered_users={"User1":"1234"}
-    
-
-    def __init__(self,starting_price) -> None:
+    def __init__(self,starting_price):
         self.starting_price = starting_price
         self.comission_bids = []
-        self.live_bidder =[]
-        self.bidder_reference={"admin1":3,"C":1,"User1":2,"SysAdmin":4}
-        self.registered_users = {"User1":"123"}
+        self.live_bidder ={}
+        self.user_reference={"admin1":3,"C":1,"User1":2,"SysAdmin":4,"L":0}
+        self.registered_users = {"User1":"1234"}
     
     def authentication_user(self,username,password):
         return username in self.registered_users and self.registered_users[username] == password
@@ -22,36 +15,53 @@ class AuctionHouse:
     def place_live_bids(self,bidder_id,bid_amount):
         self.live_bidder.append((bidder_id,bid_amount))
 class AuctionAdmin(AuctionHouse):
-    def __init__(self,starting_price,comission_bids,live_bidder,bidder_reference):
-        AuctionHouse.__init__(self,starting_price,comission_bids,live_bidder,bidder_reference)    
-        self.admin_user={"admin1":"123"}
+    def __init__(self,starting_price=0):
+        super().__init__(starting_price)    
+        self.admin_user={"admin1":"1234"}
 
     def getAdminUser(self):
-        return self.admin_user
+        #print(self.admin_user)
+        print(self.admin_user)
+    def getAuthenticate(self,username,password):
+        return username in self.admin_user and self.admin_user[username]==password
 
 
 if __name__=="__main__":
-    auction=AuctionHouse(int(input("Give starting price:")))
-    user_name=str(input("enter username (Hint user name is User1):"))
-
-    number_of_commissionbidder=int(input("enter the number of Bidder as Commision:"))
-    #print(f"{user_name} \n and {number_of_commissionbidder}")
-    for x in range(number_of_commissionbidder):
-        # auction.make_commission_bid(str(input("enter A B C")),int(input("enter number:")))
-        commission_id = input("enter X Y Z or A B :")
-        bid_amount=int(input("enter number:"))
-        if commission_id.isdigit():
-            print("please enter string! Warning!")
-            commission_id = input("enter X Y Z or A B :")
-            if commission_id.isdigit():
-                print("please restart the program")
-                break
-        elif isinstance(bid_amount,str):
-            print("Please enter number!Warning!")
-            bid_amount=int(input("enter number:"))
-            if isinstance(bid_amount,str):
-                print("please restart the program")
-                break
+    action=int(input("Enter 1 for use as admin or O as SystemAdmin"))
+    if action==1:
+        username=input("Enter Admin username:")
+        password=input("Enter Admin password:")
+        auctionAdmin=AuctionAdmin(starting_price=300)
+        if(auctionAdmin.getAuthenticate(username,password)):
+            print(auctionAdmin.getAdminUser())
+            #auction started!
+            auction=AuctionHouse(int(input("Give starting price:")))
+            number_of_commissionbidder=int(input("enter the number of Bidder as Commision:"))
+            #print(f"{user_name} \n and {number_of_commissionbidder}")
+            for x in range(number_of_commissionbidder):
+                # auction.make_commission_bid(str(input("enter A B C")),int(input("enter number:")))
+                commission_id = input("Enter Bidder commission as one Character name like A B C :")
+                bid_amount=int(input("Enter commision:"))
+                if commission_id.isdigit():
+                    print("please enter string! Warning next time abort!")
+                    commission_id = input("Enter Bidder commission as one Character name like A B C :")
+                    if commission_id.isdigit():
+                        print("please restart the program")
+                        break
+                elif isinstance(bid_amount,str):
+                    print("Please enter commision as a number/digit !Warning next time abort!")
+                    bid_amount=int(input("Enter commision:"))
+                    if isinstance(bid_amount,str):
+                        print("please restart the program")
+                        break
+                else:
+                    auction.make_commission_bid(commission_id,bid_amount,username)   
+                    #print(auction.comission_bids,auction.bidder_reference.admin1)
         else:
-            auction.make_commission_bid(commission_id,bid_amount)   
-            #print(auction.comission_bids,auction.bidder_reference.admin1)
+            print("Authentication Error") 
+
+    elif action==2 :
+        pass
+    else:
+        print("system aborted")
+
